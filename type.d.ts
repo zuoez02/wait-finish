0a2b93b2f96e30a512ddbc3313aa7f6b154c907f
Luto Yvan <zuoez02@outlook.com>
2024-06-07 01:41:44 +0000

    fix type name

--- type.d.ts
+++ type.d.ts
@@ -1,4 +1,4 @@
-declare module 'wait-for' {
+declare module 'wait-finish' {
     const exp: {
         wait:  (event: string, callback: (args: any) => any) => Promise<any>;
         finish: (event: string, result: any) => void;
